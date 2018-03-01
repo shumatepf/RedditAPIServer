1a57abe18a8e04de61219bd96d6b701a4e497bfe
Peter Shumate <shumatpf@dukes.jmu.edu>
2018-02-28 23:57:57 -0500

    Meant to be in last commit, getting over my insufficient knowledge of how git works.

--- server.js
+++ server.js
@@ -1,43 +1,31 @@
 // server
 const request = require('request');
 const express = require('express');
-const bodyParser = require('body-parser');
 
 const url = 'https://www.reddit.com/r/space/.json';
 
 const app = express();
-app.use(bodyParser.urlencoded({ extended: true }));
 
 const port = 8080;
 var posts = [];
 
-//run();
-
-console.log('request is going to happen');
-
+//pulls out the post information from the reddit json object
 request({
   url: url,
   json: true
-}, function(err, response, body) {
+}, async function(err, response, body) {
   if(err || response.statusCode != 200) {
     console.log(err);
   } else {
-    console.log('finished');
     posts = body.data.children;
     run();
   }
 });
 
+//runs the server. only called once the request is completed
 function run() {
-  //console.log('require is going to be invoked');
-  //let posts = require('./apps/parsejson.js')(request, url);
-  console.log('finished should be above this');
   require('./apps/routes.js')(app, posts);
   app.listen(port, () => {
     console.log('We are live on ' + port);
   });
-}
-
-function callback(){
-  console.log('finished');
 }
