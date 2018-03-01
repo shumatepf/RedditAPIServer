04316249a7ac1725c62faab3def1791cc09307eb
Peter Shumate <shumatpf@dukes.jmu.edu>
2018-02-28 23:56:24 -0500

    Spring cleaning. Add comments and clean code.

--- apps/routes.js
+++ apps/routes.js
@@ -1,23 +1,21 @@
 module.exports = function(app, posts) {
-  app.get('/redditer/:search', (req, res) => {
-    console.log('get is running');
-    //var posts = await require('./apps/parsejson.js')(request, url);
+  app.get('/redditer/:search', async (req, res) => {
     var search = req.params.search;
     res.write('<h1>Posts from /r/space Containing: ' + search + '</h1>');
+
+    //prints data from each post that corresponds with the search term
     for (i = 0; i < posts.length; i++) {
       var title = posts[i].data.title;
-      var author = posts[i].data.author;
-      var score = posts[i].data.score;
-      var numcomments = posts[i].data.num_comments;
-      var commentslink = 'https://www.reddit.com' + posts[i].data.permalink;
-      if (title.toLowerCase().includes(search)){
+      if (title.toLowerCase().includes(search)) {
+        var commentslink = 'https://www.reddit.com' + posts[i].data.permalink;
 	res.write('<b>' + title + '</b><br>');
-	res.write('Author: ' + author + '<br>');
-	res.write('Score: ' + score + '<br>');
-	res.write('<a href=' + commentslink + '>' +  numcomments + ' Comments</a><br><br>');
+	res.write('Author: ' + posts[i].data.author + '<br>');
+	res.write('Score: ' + posts[i].data.score + '<br>');
+	res.write('<a href=' + commentslink + '>' + 
+          posts[i].data.num_comments + ' Comments</a><br><br>');
       }
     }
-    res.write(search);
+
     res.end();
   });
 }
